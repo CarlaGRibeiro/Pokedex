01ae8f75f6f8b0ddf251ad2ee4b660dd1d83eb92
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2023-01-06 16:01:03 -0300

    projeto finalizado.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,13 @@ const pokemonName = document.querySelector('.pokemom_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search')
+const btnRetroceder = document.querySelector('.btn-prev');
+const btnAvancar = document.querySelector('.btn-next');
+
+let searchPokemon = 1;
+
 const fetchPokemon = async (pokemon) => { 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
     if(APIResponse.status ===200){
         const data = await APIResponse.json();
@@ -20,13 +25,15 @@ const renderPokemon = async (pokemon) => {
 const data = await fetchPokemon(pokemon);
 
 if(data) {
+    pokemonImage.style.display = 'block';
 pokemonName.innerHTML = data.name;
 pokemonNumber.innerHTML = data.id;
 pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-}
-else {
+searchPokemon = data.id;
+} else {
+    pokemonImage.style.display = 'none';
     pokemonNumber.innerHTML = "";
-    pokemonName.innerHTML ="Inexistente."
+    pokemonName.innerHTML ="Inexistente.";
 }
 }
 
@@ -37,16 +44,24 @@ form.addEventListener('submit', (event) => {
 
 event.preventDefault();
 
-renderPokemon(input.value);
+renderPokemon(input.value.toLowerCase());
 input.value= '';
 }
 
 );
 
+btnRetroceder.addEventListener ('click',() =>{
+    if(searchPokemon >1){
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+    }
+} 
+);
 
+btnAvancar.addEventListener ('click' , () =>{
+   searchPokemon += 1;
+   renderPokemon(searchPokemon);
+}
+);
 
-
-
-
-
-
+renderPokemon(searchPokemon);
